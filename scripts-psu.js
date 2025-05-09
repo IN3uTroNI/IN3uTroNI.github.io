769b6a40d3eb2cfd7bd80271bbe29d12965955b7
IN3uTroNI <136639106+IN3uTroNI@users.noreply.github.com>
2025-05-10 00:57:11 +0300

    Add files via upload

---
+++ scripts-psu.js
@@ -0,0 +1,4 @@
+function downloadDriver() {
+    alert("Thank you! Your download will begin shortly.");
+    window.location.href = "https://example.com/downloads/rgb-utility-v1.4.2.exe"; // Replace with actual file URL
+  }
