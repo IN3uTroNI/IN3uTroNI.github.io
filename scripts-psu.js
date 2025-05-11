38141d5a0895b5f3cbc2667c31e027faa101120a
IN3uTroNI <136639106+IN3uTroNI@users.noreply.github.com>
2025-05-11 12:56:44 +0300

    Delete scripts-psu.js

--- scripts-psu.js
+++
@@ -1,4 +0,0 @@
-function downloadDriver() {
-    alert("Thank you! Your download will begin shortly.");
-    window.location.href = "https://example.com/downloads/rgb-utility-v1.4.2.exe"; // Replace with actual file URL
-  }
